685b499ba35eaf0205afeb3c44ce00c23489ab58
Gregory Hahn <gregory.hahn@gmail.com>
2018-06-27 17:33:15 +1000

    Added support for stop_and_standby

--- script.py
+++ script.py
@@ -1,18 +1,25 @@
 import xbmc
 import urlparse
 import sys
+import time
 
 try:
         params = urlparse.parse_qs('&'.join(sys.argv[1:]))
         command = params.get('command',None)
 except:
         command = None
-        
+
 if command and command[0] == 'activate':
         xbmc.executebuiltin('CECActivateSource')
-        
+
 elif command and command[0] == 'toggle':
         xbmc.executebuiltin('CECToggleState')
 
 elif command and command[0] == 'standby':
+        xbmc.executebuiltin('CECStandby')
+
+elif command and command[0] == 'stop_and_standby':
+        if xbmc.Player().isPlaying():
+                xbmc.executebuiltin("PlayerControl(Stop)")
+                time.sleep(3)
         xbmc.executebuiltin('CECStandby')
