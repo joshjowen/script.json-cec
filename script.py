9e80bd804787664247986ddb42ed6e1f8f34b064
Jason Antman <jason@jasonantman.com>
2020-12-24 11:30:53 -0500

    fix

--- script.py
+++ script.py
@@ -8,20 +8,26 @@ try:
 except ImportError:
     from urlparse import parse_qs
 
-try:
-    params = parse_qs('&'.join(sys.argv[1:]))
-    command = params.get('command', None)
-except:
-    command = None
 
-if command and command[0] == 'activate':
-    xbmc.executebuiltin('CECActivateSource')
-elif command and command[0] == 'toggle':
-    xbmc.executebuiltin('CECToggleState')
-elif command and command[0] == 'standby':
-    xbmc.executebuiltin('CECStandby')
-elif command and command[0] == 'stop_and_standby':
-    if xbmc.Player().isPlaying():
-        xbmc.executebuiltin("PlayerControl(Stop)")
-        time.sleep(3)
-    xbmc.executebuiltin('CECStandby')
+def handle_command():
+    try:
+        params = parse_qs('&'.join(sys.argv[1:]))
+        command = params.get('command', None)
+    except:
+        command = None
+
+    if command and command[0] == 'activate':
+        xbmc.executebuiltin('CECActivateSource')
+    elif command and command[0] == 'toggle':
+        xbmc.executebuiltin('CECToggleState')
+    elif command and command[0] == 'standby':
+        xbmc.executebuiltin('CECStandby')
+    elif command and command[0] == 'stop_and_standby':
+        if xbmc.Player().isPlaying():
+            xbmc.executebuiltin("PlayerControl(Stop)")
+            time.sleep(3)
+        xbmc.executebuiltin('CECStandby')
+
+
+if __name__ == "__main__":
+    handle_command()
